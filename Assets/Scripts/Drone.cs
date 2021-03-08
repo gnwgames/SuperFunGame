813aebf6779806f3f29dd3a9946661bdc5ef980e
Greg Attra <greg.attra@gmail.com>
2021-03-07 20:02:03 -0500

    added basic movement

--- Assets/Scripts/Drone.cs
+++ Assets/Scripts/Drone.cs
@@ -3,20 +3,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(DroneInput))]
 public class Drone : MonoBehaviour
 {
     [SerializeField] private float zVelocity = 10f;
-
+    [SerializeField] private float jumpForce = 100f;
+    [SerializeField] private float horizontalForce = 100f;
+    
+    private DroneInput _input;
     private Rigidbody _rb;
 
     public void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        _input = GetComponent<DroneInput>();
     }
 
     public void Update()
     {
         _rb.velocity = new Vector3(
             _rb.velocity.x, _rb.velocity.y, zVelocity);
+    }
+
+    public void FixedUpdate()
+    {
+        if (_input.Jump())
+        {
+            _rb.AddForce(
+                transform.up * (jumpForce * Time.deltaTime));
+        }
+        if (_input.Left() > 0)
+        {
+            _rb.AddForce(-transform.right * (horizontalForce * Time.deltaTime));
+        }
+        if (_input.Right() > 0)
+        {
+            _rb.AddForce(transform.right * (horizontalForce * Time.deltaTime));
+        }
     }
 }
---
+++ Assets/Scripts/DroneInput.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class DroneInput : MonoBehaviour
+{
+    [SerializeField] private bool jump;
+    [SerializeField] private float left;
+    [SerializeField] private float right;
+
+    public void OnJump(InputValue value)
+    {
+        jump = value.isPressed;
+    }
+
+    public void OnLeft(InputValue value)
+    {
+        left = value.Get<float>();
+    }
+
+    public void OnRight(InputValue value)
+    {
+        right = value.Get<float>();
+    }
+
+    public bool Jump()
+    {
+        bool val = jump;
+        jump = false;
+        return val;
+    }
+
+    public float Left()
+    {
+        return left;
+    }
+
+    public float Right()
+    {
+        return right;
+    }
+}
